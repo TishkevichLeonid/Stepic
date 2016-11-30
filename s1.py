f7e7aa020c658e19ce87b47b00eccee02e392963
LeonidTi <leonidtishkevich@gmail.com>
2016-11-30 14:43:06 +0300

    3

--- s1.py
+++ s1.py
@@ -5,9 +5,4 @@ s.listen(1)
 while True:
     client, addr = s.accept()
     while True:
-        data = client.recv(1024)
-        if not data: break
-        client.send(data)
-        if data == "close" or data == "Close": client.close()
-        client.send(data)
-conn.close()
+ kugfout
